1ac7f7db5ab69b88dc9493705797941f69f56413
miticnikola <nikola92mitic@gmail.com>
2021-10-14 19:10:22 +0200

    adding items on list

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// 
+let form = document.querySelector('form');
+let input = document.getElementById('typeRes');
+let submitBtn = document.getElementById('submit');
+let radioTop = document.getElementById('top');
+let radioBottom = document.getElementById('bottom');
+let respList = document.getElementById('list');
+let li = document.querySelectorAll('li');
+
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    // for(let i < )
+    respList.innerHTML += `<li>${input.value}</li>`;
+    form.reset();
+});
+
+console.log(li);
